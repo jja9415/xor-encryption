f6fc54cbbb2c996c62c7e762585738508317b39a
Josh Ambrose <jja9415@g.rit.edu>
2026-09-21 18:44:07 -0400

    please work this time

--- xor.c
+++ xor.c
@@ -1,17 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
 
-void xorEncryptDecrypt(char *data, const char *key) {
-    int keyLen = strlen(key);
-    for (int i = 0; data[i] != '\0'; ++i) {
+
+void xorEncryptDecrypt(char *data, size_t dataLen, const char *key) {
+    int keyLen = (int)strlen(key);
+    for (size_t i = 0; i < dataLen; ++i) {
         data[i] ^= key[i % keyLen];
+    }
+}
+void readLine(char *buffer, int size) {
+    if (fgets(buffer, size, stdin) != NULL) {
+        buffer[strcspn(buffer, "\n")] = '\0';
+    }
+    else {
+        buffer[0] = '\0';
     }
 }
 
 int main() {
     char choice[10];
     printf("Enter 'encrypt' or 'decrypt': ");
-    scanf("%s", choice);
+    readLine(choice, sizeof(choice));
 
     if (strcmp(choice, "encrypt") == 0) {
         char message[256];
@@ -23,19 +33,19 @@ int main() {
         message[strcspn(message, "\n")] = '\0';
 
         printf("Enter the filename to save encrypted data: ");
-        scanf("%s", filename);
+        readLine(filename, sizeof(filename));
 
         printf("Enter the encryption key: ");
-        scanf("%s", key);
+        readLine(key, sizeof(key));
 
         FILE *file = fopen(filename, "wb");
         if (file == NULL) {
             perror("Error opening file for writing");
             return 1;
         }
-
-        xorEncryptDecrypt(message, key);
-        fwrite(message, sizeof(char), strlen(message), file);
+        size_t msgLen = strlen(message);
+        xorEncryptDecrypt(message, msgLen, key);
+        fwrite(message, sizeof(char), msgLen, file);
 
         fclose(file);
     } else if (strcmp(choice, "decrypt") == 0) {
@@ -43,10 +53,10 @@ int main() {
         char key[256];
 
         printf("Enter the filename to read encrypted data: ");
-        scanf("%s", filename);
+		readLine(filename, sizeof(filename));
 
         printf("Enter the decryption key: ");
-        scanf("%s", key);
+		readLine(key, sizeof(key));
 
         FILE *file = fopen(filename, "rb");
         if (file == NULL) {
@@ -60,8 +70,8 @@ int main() {
 
         fclose(file);
 
-        xorEncryptDecrypt(buffer, key);
-        printf("Decrypted data: %s\n", buffer);
+        xorEncryptDecrypt(buffer, bytesRead, key);
+        printf("Decrypted data: %s\n", buffer); 
     } else {
         printf("Invalid choice. Please enter 'encrypt' or 'decrypt'.\n");
         return 1;
